a6d42229d605405819f98d729be1e50ffbfdbc84
Sky Zhang <sky.atomse@gmail.com>
2019-07-03 17:19:26 +0800

    add vasp paw pbe pot

---
+++ gaseio/format_string/__basic__.py
@@ -0,0 +1,26 @@
+"""
+
+
+basic format string
+
+
+"""
+
+
+
+
+BASIC_PRIMITIVE_DATA_FORMAT_STRING = {
+    r'<filename>(.*)</filename>' : {
+        'selection' : -1,
+        'important' : False,
+        'key' : 'file/origin_filename', 
+        },
+    r'<hostname>(.*)</hostname>' : {
+        'selection' : -1,
+        'important' : False,
+        'key' : 'file/origin_hostname',
+        },
+}
+
+
+
--- gaseio/gase_writer.py
+++ gaseio/gase_writer.py
@@ -16,20 +16,31 @@ from .regularize import regularize_arrays
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_TEMPLATE_DIR = 'input_templates'
 
-jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(BASEDIR, INPUT_TEMPLATE_DIR)),
-                                    lstrip_blocks=True)
+jinja_loader = jinja2.FileSystemLoader(os.path.join(BASEDIR, INPUT_TEMPLATE_DIR))
+jinja_environment = jinja2.Environment(loader=jinja_loader, lstrip_blocks=True)
 
 def islist(value):
     return isinstance(value, list)
 
 
+def include_vasppot(fname):
+    print(fname)
+    potpaw_PBE_dir = 'potpaw_PBE'
+    fname = os.path.join(BASEDIR, potpaw_PBE_dir, str(fname), 'POTCAR')
+    _tmp = jinja_loader.get_source(jinja_environment, fname)[0]
+    print(_tmp)
+    return jinja2.Markup(_tmp)
+
+
+jinja_environment.globals['include_vasppot'] = include_vasppot
+jinja_environment.trim_blocks = True
+jinja_environment.filters.update({
+    'islist': islist,
+})
+
+
 def generate_input_content(arrays, filetype):
     template_name = filetype + '.j2'
-    jinja_environment.trim_blocks = True
-    jinja_environment.filters.update({
-        'islist': islist,
-    })
-
     template = jinja_environment.get_template(template_name)
     if not isinstance(arrays, dict) and hasattr(arrays, 'get_positions'):
         module_name = arrays.__class__.__module__
@@ -46,6 +57,7 @@ def generate_input_content(arrays, filetype):
         else: # gase
             arrays = arrays.arrays
     regularize_arrays(arrays)
+    # print(arrays['symbols'])
     if isinstance(arrays, list):
         output = template.render(arrays=arrays, arrays_json=json_tricks.dumps(arrays))
     else:

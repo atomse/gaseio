2469af79e0c30079361619a5e3cac2fc234e5c7f
Sky Zhang <sky.atomse@gmail.com>
2019-09-20 10:15:11 +0800

    2.5.6

--- gaseio/__init__.py
+++ gaseio/__init__.py
@@ -8,7 +8,7 @@ from .main import read_preview, write_preview, preview, get_write_content
 from .main import list_supported_write_formats
 
 
-__version__ = '2.5.5'
+__version__ = '2.5.6'
 
 def version():
     return __version__

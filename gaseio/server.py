0c3045d6ba9852e002b424044a85a779a09431ce
Sky Zhang <sky.atomse@gmail.com>
2019-11-08 16:15:09 +0800

    fire update

--- gaseio/server.py
+++ gaseio/server.py
@@ -20,7 +20,8 @@ PORT = os.environ.get("GASEIO_PORT", DEFAULT_GASEIO_PORT)
 MAX_CORE = int(os.environ.get("GASEIO_MAX_CORE", DEFAULT_GASEIO_MAX_CORE))
 
 
-def main(max_core: int = MAX_CORE):
+def main(address: str = '127.0.0.1', port: int = PORT,
+         max_core: int = MAX_CORE):
     http_server = HTTPServer(WSGIContainer(app))
     http_server.listen(PORT, '127.0.0.1')
     http_server.start(max_core)
@@ -30,6 +31,8 @@ def main(max_core: int = MAX_CORE):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--address', type=str, default='127.0.0.1')
+    parser.add_argument('-p', '--port', type=int, default=PORT)
     parser.add_argument('-n', '--max-core', type=int, default=MAX_CORE)
     args = parser.parse_args()
-    main(args.max_core)
+    main(args.address, args.port, args.max_core)

c49c6ba03c87e13433d3a711a877079a71aeb895
Sky Zhang <sky.atomse@gmail.com>
2019-11-30 01:37:21 +0800

    fire update

--- gaseio/server.py
+++ gaseio/server.py
@@ -16,25 +16,25 @@ from gaseio.app import app
 
 DEFAULT_GASEIO_PORT = 5000
 DEFAULT_GASEIO_MAX_CORE = 4
+LOCALHOST = '127.0.0.1'
 PORT = os.environ.get("GASEIO_PORT", DEFAULT_GASEIO_PORT)
 MAX_CORE = int(os.environ.get("GASEIO_MAX_CORE", DEFAULT_GASEIO_MAX_CORE))
 
 
-def tornado_server(address: str = '127.0.0.1', port: int = PORT,
+def tornado_server(address: str = LOCALHOST, port: int = PORT,
                    max_core: int = MAX_CORE):
-    try:
-        http_server = HTTPServer(WSGIContainer(app))
-        http_server.listen(port, address)
-        http_server.start(max_core)
-        # IOLoop.instance().start()
-        IOLoop.current().start()
-    except KeyboardInterrupt:
-        IOLoop.current().stop()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(port, address)
+    http_server.start(max_core)
+    # ioloop = IOLoop.current()
+    ioloop = IOLoop.instance()
+    # ioloop.add_callback(ioloop.stop)
+    ioloop.start()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--address', type=str, default='127.0.0.1')
+    parser.add_argument('-a', '--address', type=str, default=LOCALHOST)
     parser.add_argument('-p', '--port', type=int, default=PORT)
     parser.add_argument('-n', '--max-core', type=int, default=MAX_CORE)
     parser.add_argument('--debug', action='store_true')
